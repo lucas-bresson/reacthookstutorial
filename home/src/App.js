e14625d5aa43f9a098baf67055fffda972a91739
lucas-bresson <contact.bresson@gmail.com>
2019-11-20 17:05:40 +0100

    completed 1st section & created custom hook useFetch

--- home/src/App.js
+++ home/src/App.js
@@ -1,5 +1,38 @@
-import React from "react";
+import React, { useState } from 'react';
+import Joke from './Joke';
+import Stories from './Stories';
 
-const App = () => <div className="App" />;
+function App() {
+  const [userQuery, setUserQuery] = useState('');
+
+  const updateUserQuery = event => {
+    console.log('userQuery', userQuery);
+    setUserQuery(event.target.value);
+  };
+
+  const handleKeyPress = event => {
+    if (event.key === 'Enter') {
+      searchQuery();
+    }
+  };
+
+  const searchQuery = () => {
+    window.open(`https://google.com/search?q=${userQuery}`, '_blank');
+  };
+
+  return (
+    <div className="App">
+      <h1>Hello John</h1>
+      <div className="form">
+        <input value={userQuery} onChange={updateUserQuery} onKeyPress={handleKeyPress} />
+        <button onClick={searchQuery}>Search</button>
+      </div>
+      <hr />
+      <Joke />
+      <hr />
+      <Stories />
+    </div>
+  );
+}
 
 export default App;
